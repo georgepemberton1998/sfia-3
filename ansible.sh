8709d8721cfb154a5a1c0e53b25523d0c375f61d
georgepemberton1998 <67062260+georgepemberton1998@users.noreply.github.com>
2020-10-16 13:51:40 +0100

    Delete ansible.sh

--- ansible.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd ~/sfia-3/ansible
-
-ansible-playbook -i {inv 1} {playbook1}
-ansible-playbook -i {inv 2} {playbook2}
-ansible-playbook -i {inv 3} {playbook3}
-
-echo "Successfully completed Ansible playbooks"
