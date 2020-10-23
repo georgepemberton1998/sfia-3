97c889c9c3836b98aab9d88efa01de302067e6af
Macaulay Farrell <Maccpr7@gmail.com>
2020-10-23 23:03:34 +0100

    update to frontend starter

--- frontend/src/components/ViewTickets.jsx
+++ frontend/src/components/ViewTickets.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const ViewTickets = () => {
-    axios.get('http://localhost:8082/ticket/getTickets')
-    .then((response) => {
-        console.log(response.data);
-    });
-    return (
+    const [tickets, setTickets] = useState([]);
+    const updateUrl = "update/";
+    const deleteUrl = "delete/";
+  
+    useEffect(() => {
+      axios
+        .get("http://localhost:8082/ticket/getTickets")
+        .then((data) => data)
+        .then((result) => {
 
-        <p>Tickets here</p>
+            setTickets(result.data);
+
+  
+          }, 
+        );
+    }, []);
+    return (
+        <div>
+        {console.log(tickets)}
+        {tickets.map((ticket) => (
+          <li key={ticket.id}>
+            Title: {ticket.title}<br/>
+            Topic: {ticket.topic}<br/>
+            Author: {ticket.author} <br/>
+            Description: {ticket.description}<br/>
+            Urgency: {ticket.urgency}<br/>
+
+           <a href={updateUrl + ticket.id}>Update Ticket</a><br/>
+           <a href={deleteUrl + ticket.id}>Delete Ticket</a>
+           
+          </li>
+        ))}
+    
+        </div>
 
     );
 }
