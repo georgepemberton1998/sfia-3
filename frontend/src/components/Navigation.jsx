1c3c7441805a1dfeff9b40027c196b100321a045
Macaulay Farrell <Maccpr7@gmail.com>
2020-10-26 17:39:38 +0000

    add logo to navigation

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from './images/qa-logo.png';
 
 const Navigation = () => {
     return (
         <>
         <nav>
+            <a href="http://qa.com/"><img className="logo" src={logo} alt="logo"></img></a>
             <a href="/">View Tickets</a>
             <a href="/createTicket">Create Ticket</a>
         </nav>
