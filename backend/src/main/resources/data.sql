075167a342d9972bb4276beb28b155a3c79ae3f8
KeenanB <k.bratley626@gmail.com>
2020-10-29 10:18:13 +0000

    data.sql file created for testing

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO `Ticket`
+(`title`,
+`author`,
+`description`,
+`urgency`,
+`topic`,
+`completed`)
+VALUES
+('nginx issue',
+'Macaulay Farrell',
+'Help meh pls',
+'very urgent',
+'SoftwareDev',
+0);
